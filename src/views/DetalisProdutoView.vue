5edcfb04668f5e53510a47bfb9e8b491894024eb
Bulla <pedro-bulla@hotmail.com>
2023-06-19 10:39:31 -0300

    caroseu colocado
    falta editar

---
+++ src/views/DetalisProdutoView.vue
@@ -0,0 +1,24 @@
+<template>
+    <Headers/>
+    <Carousei/>
+    
+    </template>
+
+<script>
+import { Options, Vue } from 'vue-class-component';
+import Headers from '../components/Header.vue'
+import Carousei from '../components/Anuncios.vue'
+
+
+@Options({
+     components:{
+       Headers,
+       Carousei
+
+     },
+})
+export default class HomeView extends Vue {
+
+  
+  }
+</script>
