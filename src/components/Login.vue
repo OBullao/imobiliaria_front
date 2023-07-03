fffb31ef9c2adfdd4a0de1f472113b2bb77d3ef1
Bulla <pedro-bulla@hotmail.com>
2023-07-03 20:09:35 -0300

    mudei
    ad

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,9 +2,9 @@
   <div class="login-container d-flex align-items-center justify-content-center">
     <div class="login-form">
       <h3 class=" fundo text-center mb-4">Login</h3>
-      <form>
+      <form class="fundo do formes">
         <div class="fundo mb-3">
-          <label for="email" class=" form-label">Email</label>
+          <label for="email" class=" fundo form-label">Email</label>
           <input type="email" class="form-control" id="email" placeholder="Enter email">
         </div>
         <div class="fundo mb-3">
@@ -27,11 +27,13 @@
   padding: 20px;
   background-color: #fff;
   border-radius: 5px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+  box-shadow: 0 2px 5px rgba(255, 0, 0, 0.3);
 }
 .fundo{
    background-color: rgb(255, 252, 194);
 }
+
+
 </style>
 
 
