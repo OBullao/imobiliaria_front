d779a61cdaf79d145d5cc1fa50e9493a5144a87e
dogogh <quadros1919@gmail.com>
2023-07-04 15:48:21 -0300

    descricao(tem q arrumar a foto)

--- src/client/Descricao.client.ts
+++ src/client/Descricao.client.ts
@@ -1,60 +1,56 @@
-import { DescricaoModel } from "@/model/DescricaoModel";
 import axios, { AxiosInstance } from "axios";
+import { DescricaoModel } from "@/model/DescricaoModel";
 
-export class DescricaoClient {
-    private axiosClient: AxiosInstance;
+class DescricaoClient {
+  private axiosClient: AxiosInstance;
 
   constructor() {
     this.axiosClient = axios.create({
-      baseURL: "http://localhost:8080/api/descricao",
-      headers: { "Content-type": "application/json" },
+      baseURL: 'http://localhost:8081/api/descricao',
+      headers: { 'Content-type': 'application/json' }
     });
   }
   public async findById(id: number): Promise<DescricaoModel> {
     try {
-      return (await this.axiosClient.get<DescricaoModel>(`/${id}`)).data;
+      return (await this.axiosClient.get<DescricaoModel>(`/lista/id/${id}`)).data;
     } catch (error: any) {
       return Promise.reject(error.response);
     }
   }
 
-  public  async listAll() : Promise<DescricaoModel[]>{
-    try{
-        return (await this.axiosClient.get<DescricaoModel[]> ('/lista')).data
-    }
-    catch(error: any){
-        return Promise.reject(error.response)
-    }
-}
-
-
-
-
-
-
-
-
-  public async cadastrar(cadastrar: DescricaoModel): Promise<void> {
+  public async listaAll(): Promise<any> {
     try {
-      return await this.axiosClient.post("/", cadastrar);
+      return (await this.axiosClient.get<DescricaoModel[]>(`/lista`)).data;
     } catch (error: any) {
       return Promise.reject(error.response);
     }
   }
-  public async editar(editar: DescricaoModel): Promise<void> {
+
+  public async cadastrar(cadastro: DescricaoModel): Promise<string> {
     try {
-      return (await this.axiosClient.put(`/${editar.id}`, editar)).data;
+      return (await this.axiosClient.post<string>(``, cadastro)).data;
     } catch (error: any) {
       return Promise.reject(error.response);
     }
   }
-  public async desativar(deasativar: DescricaoModel): Promise<void> {
+
+  public async editar(id: number, editar: DescricaoModel): Promise<string> {
     try {
-      return (await this.axiosClient.put(`/desativar/${deasativar.id}`, deasativar))
-        .data;
+      return (await this.axiosClient.put<string>(`/${id}`, editar)).data;
+    } catch (error: any) {
+      return Promise.reject(error.response);
+    }
+  }
+
+  public async excluir(id: number): Promise<string> {
+    try {
+      return (await this.axiosClient.delete<string>(`/delete/${id}`)).data;
     } catch (error: any) {
       return Promise.reject(error.response);
     }
   }
 }
-export default new DescricaoClient;
+
+export default new DescricaoClient();
+
+
--- src/components/DescricaoForm.vue
+++ src/components/DescricaoForm.vue
@@ -1,40 +1,94 @@
 <template>
-    <div>
-      <h1>Formulário - Descrição do Apartamento</h1>
-      <form >
-        <div class="form-group">
-          <label for="cadastro">Data de Cadastro:</label>
-          <input type="datetime-local" ref="cadastro" class="form-control" id="cadastro" required>
+  <div>
+    <h1>Formulário - Descrição </h1>
+    <form @submit.prevent="onClickCadastrar">
+      <div v-if="mensagem.ativo" class="row">
+        <div class="col-md-12 text-start">
+          <div :class="mensagem.css" role="alert">
+            <strong>{{ mensagem.titulo }}</strong> {{ mensagem.mensagem }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="edicao">Data de Edição:</label>
-          <input type="datetime-local" ref="edicao" class="form-control" id="edicao">
-        </div>
-        <div class="form-group">
-          <label for="ativo">Ativo:</label>
-          <select ref="ativo" class="form-control" id="ativo">
-            <option value="true">Sim</option>
-            <option value="false">Não</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="descricao">Descrição:</label>
-          <textarea ref="descricao" class="form-control" id="descricao" placeholder="Digite a descrição" required></textarea>
-        </div>
-        <div class="form-group">
-          <label for="fotos">Fotos:</label>
-          <input type="file" ref="fotos" class="form-control" id="fotos" accept="image/*" required>
-        </div>
-        <button type="submit" class="btn btn-primary">Enviar</button>
-      </form>
-    </div>
-  </template>
-  
+      </div>
+
+      <div class="form-group">
+        <label for="descricao">Descrição:</label>
+        <textarea :disabled="form === 'excluir' ? disabled : false" class="form-control" v-model="descricao.descricao"></textarea>
+      </div>
+
+      <div class="form-group">
+        <label for="fotos">Fotos:</label>
+        <input type="text" class="form-control" :disabled="form === 'excluir' ? disabled : false" v-model="descricao.fotos">
+      </div>
+
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+  </div>
+</template>
+
 <script lang="ts">
-import { defineComponent } from "vue";
-import { RouterLink } from "vue-router";
+import DescricaoClient from '@/client/Descricao.client';
+import { DescricaoModel } from '@/model/DescricaoModel';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: "descricaoForm",
+  name: 'DescricaoForm',
+  data() {
+    return {
+      descricao: new DescricaoModel(),
+      mensagem: {
+        ativo: false,
+        titulo: "",
+        mensagem: "",
+        css: ""
+      },
+      disabled: false
+    };
+  },
+  computed: {
+    id() {
+      return this.$route.query.id;
+    },
+    form() {
+      return this.$route.query.form;
+    }
+  },
+  mounted() {
+    if (this.id !== undefined) {
+      this.findById(Number(this.id));
+    }
+  },
+  methods: {
+    onClickCadastrar() {
+      // Aqui você pode incluir a lógica para enviar a foto para o servidor junto com a descrição
+
+      DescricaoClient.cadastrar(this.descricao)
+        .then(success => {
+          this.descricao = new DescricaoModel();
+          this.mensagem.ativo = true;
+          this.mensagem.mensagem = success;
+          this.mensagem.titulo = "Parabéns. ";
+          this.mensagem.css = "alert alert-success alert-dismissible fade show";
+        })
+        .catch(error => {
+          this.mensagem.ativo = true;
+          this.mensagem.mensagem = error;
+          this.mensagem.titulo = "Erro. ";
+          this.mensagem.css = "alert alert-danger alert-dismissible fade show";
+        });
+    },
+    findById(id: number) {
+      DescricaoClient.findById(id)
+        .then(success => {
+          this.descricao = success;
+        })
+        .catch(error => {
+          this.mensagem.ativo = true;
+          this.mensagem.mensagem = error;
+          this.mensagem.titulo = "Erro. ";
+          this.mensagem.css = "alert alert-danger alert-dismissible fade show";
+        });
+    }
+  }
 });
 </script>
